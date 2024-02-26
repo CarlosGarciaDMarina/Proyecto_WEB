a747de5750a2056ac689a21331971f9605cc293b
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-02-26 14:50:00 +0100

    Arreglo final

--- JS/main.js
+++ JS/main.js
@@ -157,16 +157,4 @@ $(document).ready(function(){
       scrollToTopOnError: true
     });
   }
-
-
-
-/**<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script> */
-
-
-
-
-
-
-
-
 });
